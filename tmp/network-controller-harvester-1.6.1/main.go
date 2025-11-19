e41de7cf7bcfab46c06d91961fe047f215de0ac1
derhornspieler <15236687+derhornspieler@users.noreply.github.com>
2025-11-19 11:05:47 -0500

    removed files not needed anymore.

--- tmp/network-controller-harvester-1.6.1/main.go
+++
@@ -1,8 +0,0 @@
-//go:generate go run pkg/codegen/cleanup/main.go
-//go:generate /bin/rm -rf pkg/generated
-//go:generate go run pkg/codegen/main.go
-//go:generate /bin/bash scripts/generate-manifest
-//go:generate goimports -w ./pkg/apis
-//go:generate goimports -w ./pkg/generated
-
-package main
